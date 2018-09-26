5fa0d7945a4cba8d4de8e8d7ac5bdf5b7ea6331d
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-26 12:52:06 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,10 +25,12 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	monopoly = {
+ 	monopoly = {
 	  :railroads => {
 	    :pieces => 4,
-	    :names => {
+	    :names => {},
+	    :rent_in_dollars => {
+	      :one_piece_owned => 25
 	    }
 	  }
 	}
